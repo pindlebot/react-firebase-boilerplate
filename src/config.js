e4dbca05c3d2bf7acc7d790373e3b27ffaba2140
Ben <bgardner620@gmail.com>
2017-07-13 04:07:29 -0400

    new

---
+++ src/config.js
@@ -0,0 +1,14 @@
+// https://console.firebase.google.com/
+// Click "Add to web app" 
+// Copy + paste config
+
+var config = {
+  apiKey: "your-api-key",
+  authDomain: "your-app.firebaseapp.com",
+  databaseURL: "https://your-app.firebaseio.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "your-messaging-id"
+};
+
+module.exports = config
